9ed3a7bcfda39e7e27e94e80b96778da77ed14bf
mrnkwar-sys <mr.nkwar@iesnervion.es>
2025-12-04 14:39:43 +0100

    advance

--- Unit03/src/arrays/Ejercicio05.java
+++ Unit03/src/arrays/Ejercicio05.java
@@ -11,13 +11,33 @@ public class Ejercicio05 {
 		//The numbers introduced by the user
 		int num;
 		
+		//The sum of all the numbers
+		int sum;
+		
+		//The number to make the comparison
+		int compNum;
+		
+		//The smallest number
+		int small;
+		
+		//The largest number
+		int big;
+		
 		//The table where the numbers will be kept
 		int table[] = new int [10];
 		
-		//The console waits for the user to introduce 10 numbers
+		//The console waits for the user to introduce 10 numbers, which will be kept in the table
 		for (int i = 0; i < table.length; i++) {
+			System.out.println("Introduzca un número:");
+			num = sc.nextInt();
+			table[i] = i;
+			sum += num;
 			
+			}
 		}
+		
+		//We sum all the numbers of the table just made
+		
 
 	}
 
